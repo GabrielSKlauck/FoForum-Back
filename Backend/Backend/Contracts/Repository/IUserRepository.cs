0036fcccef9d6508eb321e7c8cd656e78b2f98fd
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-05-11 19:45:38 -0300

    IUseRepository feito, classes de infraestrutura criados

---
+++ Backend/Backend/Contracts/Repository/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Backend.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Contracts.Repository
+{
+    public interface IUserRepository
+    {
+        Task AddUser(UserDTO user);
+
+        Task GetUser(int id);
+
+        Task UpdateUser(UserDTO user);
+
+        Task DeleteUser(int id);
+    }
+}
