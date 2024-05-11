89190f86e285ebc737d4f7848185fd2ce325f94f
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-05-10 23:09:59 -0300

    Criação de entidades

---
+++ Backend/Backend/Entity/AnswersEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entity
+{
+    public class AnswersEntity
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Posted { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
